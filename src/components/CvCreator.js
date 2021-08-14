a29667efedac903018b2f80b83dafb4ecc35a307
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-14 11:47:19 +0200

    Added a way to switch between windows

--- src/components/CvCreator.js
+++ src/components/CvCreator.js
@@ -1,18 +1,69 @@
 import React from "react";
 import Personal from "./Personal";
+import Education from "./Education";
+import Work from "./Work";
 
 class CvCreator extends React.Component
 {
+	constructor()
+	{
+		super()
+
+		this.state =
+		{
+			window:
+			{
+				personal: true,
+				education: false,
+				work: false,
+			}
+		}
+
+		this.changeWindow = this.changeWindow.bind(this);
+	}
+
+	changeWindow = window => this.setState
+	(
+		{
+			window:
+			{
+				personal: false,
+				education: false,
+				work: false,
+				[window]: true,
+			}
+		}
+	)
+
 	render()
 	{
 		return(
 			<div className="cv-creator">
 				<div className="buttons-container">
-					<div className="button left">Personal Info</div>
-					<div className="button middle unselected">Education</div>
-					<div className="button right unselected">Work Experience</div>
+					<div
+					className={`button left ${this.state.window.personal ? null : "unselected"}`}
+					onClick={() => this.changeWindow("personal")}
+					>Personal Info</div>
+
+					<div
+					className={`button middle ${this.state.window.education ? null : "unselected"}`}
+					onClick={() => this.changeWindow("education")}
+					>Education</div>
+					
+					<div
+					className={`button right ${this.state.window.work ? null : "unselected"}`}
+					onClick={() => this.changeWindow("work")}
+					>Work Experience</div>
 				</div>
-				<Personal setPersonalInfo={this.props.setPersonalInfo} personalInfo={this.props.personalInfo}/>
+				{
+					this.state.window.personal 
+					? <Personal setPersonalInfo={this.props.setPersonalInfo} personalInfo={this.props.personalInfo}/> 
+					: this.state.window.education
+					? <Education/>
+					: this.state.window.work
+					? <Work/>
+					: null
+				}
 			</div>
 		)
 	}
