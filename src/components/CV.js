9461f5137aa95f1d4f8f3917f6604c896b908b5b
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-15 09:36:26 +0200

    Added education to the CV page

--- src/components/CV.js
+++ src/components/CV.js
@@ -13,6 +13,22 @@ class CV extends React.Component
 						<h1>{this.props.info.personalInfo.name}</h1>
 						<h2>{this.props.info.job}</h2>
 					</div>
+					<div className="cv-body">
+						<div className="cv-description-box">
+							<h1>Education</h1>
+							{
+								this.props.info.education.map
+								(edu =>
+									<div className="cv-education" key={edu.key}>
+										<h4>{edu.year}</h4>
+										<h2>{edu.degree}</h2>
+										<h3>{edu.titleOfStudy}</h3>
+										<h3>{edu.school}</h3>
+									</div>
+								)
+							}
+						</div>
+					</div>
 				</div>
 			</div>
 		)
