e30d5fe573439e5852ad283fa127b8609e33dd57
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-17 13:36:09 +0200

    Converted AddButton class component to a functional component

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,15 +1,8 @@
 import React from "react";
 
-class AddButton extends React.Component
-{
-	render()
-	{
-		return(
-			<div>
-				{this.props.property.length < this.props.length ? <div className="add-button" onClick={this.props.action}>+</div> : null}
-			</div>
-		)
-	}
-}
+const AddButton = props =>
+<div>
+	{props.property.length < props.length ? <div className="add-button" onClick={props.action}>+</div> : null}
+</div>
 
 export default AddButton;
