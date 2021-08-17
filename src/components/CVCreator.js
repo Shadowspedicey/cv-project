4eeb88921370bd6b0a6d2406f5b62565c6b58bed
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-17 13:14:55 +0200

    Converted CVCreator class component to a functional component

--- src/components/CVCreator.js
+++ src/components/CVCreator.js
@@ -1,98 +1,84 @@
-import React from "react";
+import React , {useState} from "react";
 import Personal from "./windows/Personal";
 import Education from "./windows/Education";
 import Skills from "./windows/Skills";
 import Work from "./windows/Work";
 import Export from "./windows/Export";
 
-class CVCreator extends React.Component
+const CVCreator = props =>
 {
-	constructor()
-	{
-		super()
-
-		this.state =
-		{
-			window:
-			{
-				personal: true,
-				education: false,
-				skills: false,
-				work: false,
-				export: false,
-			}
-		}
-
-		this.changeWindow = this.changeWindow.bind(this);
-	}
-
-	changeWindow = window => this.setState
+	const [window, setWindow] = useState
 	(
 		{
-			window:
-			{
-				personal: false,
-				education: false,
-				skills: false,
-				work: false,
-				export: false,
-				[window]: true,
-			}
+			personal: true,
+			education: false,
+			skills: false,
+			work: false,
+			export: false,
 		}
-	)
+	);
 
-	render()
-	{
-		return(
-			<div className="cv-creator"
-			style={this.props.info.switchMenu.on ? this.props.info.switchMenu.menus.cvCreator ? null : {display: "none"} : null}>
-				<div className="buttons-container">
-					<div
-					className={`button left left-corner ${this.state.window.personal ? null : "unselected"}`}
-					onClick={() => this.changeWindow("personal")}>
-						Personal Info
-					</div>
+	const changeWindow = window => setWindow
+	(
+		{
+			personal: false,
+			education: false,
+			skills: false,
+			work: false,
+			export: false,
+			[window]: true,
+		}
+	);
 
-					<div
-					className={`button middle ${this.state.window.education ? null : "unselected"}`}
-					onClick={() => this.changeWindow("education")}>
-						Education
-					</div>
-					
-					<div
-					className={`button middle right ${this.state.window.skills ? null : "unselected"}`}
-					onClick={() => this.changeWindow("skills")}>
-						Skills
-					</div>
-
-					<div
-					className={`button right ${this.state.window.work ? null : "unselected"}`}
-					onClick={() => this.changeWindow("work")}>
-						Work Experience
-					</div>
-
-					<div
-					className={`button right right-corner ${this.state.window.export ? null : "unselected"}`}
-					onClick={() => this.changeWindow("export")}>
-						Export
-					</div>
+	return(
+		<div className="cv-creator"
+		style={props.info.switchMenu.on ? props.info.switchMenu.menus.cvCreator ? null : {display: "none"} : null}>
+			<div className="buttons-container">
+				<div
+				className={`button left left-corner ${window.personal ? null : "unselected"}`}
+				onClick={() => changeWindow("personal")}>
+					Personal Info
 				</div>
-				{
-					this.state.window.personal 
-					? <Personal setPersonalInfo={this.props.personalFns.setPersonalInfo} personalInfo={this.props.info.personalInfo}/> 
-					: this.state.window.education
-					? <Education educationFns={this.props.educationFns} education={this.props.info.education}/>
-					: this.state.window.skills
-					? <Skills skillsFns={this.props.skillsFns} job={this.props.info.job} skills={this.props.info.skills}/>
-					: this.state.window.work
-					? <Work workFns={this.props.workFns} works={this.props.info.work}/>
-					: this.state.window.export
-					? <Export/>
-					: null
-				}
+
+				<div
+				className={`button middle ${window.education ? null : "unselected"}`}
+				onClick={() => changeWindow("education")}>
+					Education
+				</div>
+				
+				<div
+				className={`button middle right ${window.skills ? null : "unselected"}`}
+				onClick={() => changeWindow("skills")}>
+					Skills
+				</div>
+
+				<div
+				className={`button right ${window.work ? null : "unselected"}`}
+				onClick={() => changeWindow("work")}>
+					Work Experience
+				</div>
+
+				<div
+				className={`button right right-corner ${window.export ? null : "unselected"}`}
+				onClick={() => changeWindow("export")}>
+					Export
+				</div>
 			</div>
-		)
-	}
+			{
+				window.personal 
+				? <Personal setPersonalInfo={props.personalFns.setPersonalInfo} personalInfo={props.info.personalInfo}/> 
+				: window.education
+				? <Education educationFns={props.educationFns} education={props.info.education}/>
+				: window.skills
+				? <Skills skillsFns={props.skillsFns} job={props.info.job} skills={props.info.skills}/>
+				: window.work
+				? <Work workFns={props.workFns} works={props.info.work}/>
+				: window.export
+				? <Export/>
+				: null
+			}
+		</div>
+	)
 }
 
 export default CVCreator;
