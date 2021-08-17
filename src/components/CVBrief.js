a81ef04952f3bae08ec3257fcdcbc90d990a556c
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-17 13:33:50 +0200

    Converted CVBrief class component to a functional component

--- src/components/CVBrief.js
+++ src/components/CVBrief.js
@@ -1,53 +1,46 @@
 import React from "react";
 
-class CVBrief extends React.Component
-{
-	render()
-	{
-		return(
-			<div className="cv-brief">
-					<div className="cv-photo">
-						<img src={this.props.personalInfo.photo} alt=""></img>
-					</div>
+const CVBrief = props =>
+<div className="cv-brief">
+	<div className="cv-photo">
+		<img src={props.personalInfo.photo} alt=""></img>
+	</div>
 
-					<div className="cv-info-box cv-contact">
-						<h2>CONTANT</h2>
-						<ul>
-							<li>
-								<span className="material-icons">phone</span>
-								<span style={{whiteSpace: "nowrap"}}>+({this.props.personalInfo.phoneNumber.area}){this.props.personalInfo.phoneNumber.number}</span>
-							</li>
-							<li>
-								<span className="material-icons">email</span>
-								<span className="cv-email">{this.props.personalInfo.email}</span>
-							</li>
-							<li>
-								<span className="material-icons">place</span>
-								<span>
-									{this.props.personalInfo.residence.city},
-									{` ${this.props.personalInfo.residence.country}`}
-								</span>
-							</li>
-						</ul>
-					</div>
+	<div className="cv-info-box cv-contact">
+		<h2>CONTANT</h2>
+		<ul>
+			<li>
+				<span className="material-icons">phone</span>
+				<span style={{whiteSpace: "nowrap"}}>+({props.personalInfo.phoneNumber.area}){props.personalInfo.phoneNumber.number}</span>
+			</li>
+			<li>
+				<span className="material-icons">email</span>
+				<span className="cv-email">{props.personalInfo.email}</span>
+			</li>
+			<li>
+				<span className="material-icons">place</span>
+				<span>
+					{props.personalInfo.residence.city},
+					{` ${props.personalInfo.residence.country}`}
+				</span>
+			</li>
+		</ul>
+	</div>
 
-					<div className="cv-info-box cv-skills">
-						<h2>SKILLS</h2>
-						<ul>
-							{this.props.skills.map(skill => <li key={skill.key}>{skill.name}</li>)}
-						</ul>
-					</div>
+	<div className="cv-info-box cv-skills">
+		<h2>SKILLS</h2>
+		<ul>
+			{props.skills.map(skill => <li key={skill.key}>{skill.name}</li>)}
+		</ul>
+	</div>
 
-					<div className="refrence-info">
-						<div>
-							<h2>REFRENCE</h2>
-							<h3>Ahmed Khaled</h3>
-							<h4>Web Developer</h4>
-						</div>
-					</div>
-				</div>
-		)
-	}
-}
+	<div className="refrence-info">
+		<div>
+			<h2>REFRENCE</h2>
+			<h3>Ahmed Khaled</h3>
+			<h4>Web Developer</h4>
+		</div>
+	</div>
+</div>
 
 export default CVBrief;
